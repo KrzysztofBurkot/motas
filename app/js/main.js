1170961576fc3d3716bde85d57939d03f5f5a6ce
Krzysztof Burkot <buryPL@gmail.com>
2017-08-15 18:47:20 +0200

    bootstrap_menu_repair_attempt_#1
    First, unfruitful attempt to fix buggy bootstrap menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
- 
+/*SMOOTH SCROLL*/
+$(document).ready(function () {
+    $('.menu_list_items').on('click', function (e) {
+        e.preventDefault();     
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top -50
+        }, 500);
+    });
+});
